551f3e136b86ab66a2a0096459dfc474e6043a6d
Hidayat <hidayetmmdff@gmail.com>
2022-03-24 16:50:19 +0400

    type safety for tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable()
 export class TasksService {
-  private tasks = ['task1', 'task2', 'task3'];
+  private tasks: Task[] = [
+    {
+      id: '1',
+      title: 'Task 1',
+      description: 'Description 1',
+      status: TaskStatus.IN_PROGRESS,
+    },
+  ];
 
   getAllTasks() {
     return this.tasks;
