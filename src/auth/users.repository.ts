9664b1c4eef9bbe213a9914c8ef0093ce612b2e3
Hidayat <hidayetmmdff@gmail.com>
2022-04-10 22:02:19 +0400

    auth module init

---
+++ src/auth/users.repository.ts
@@ -0,0 +1,12 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+@EntityRepository(User)
+export class UsersRepository extends Repository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const task = this.create({ username, password });
+    await this.save(task);
+  }
+}
