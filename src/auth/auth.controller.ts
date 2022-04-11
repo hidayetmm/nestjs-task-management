efba504e33ea991384b3c3b04e1cfa56bd96bef3
Hidayat <hidayetmmdff@gmail.com>
2022-04-11 22:14:20 +0400

    password hashing/signin implemented

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,6 +7,11 @@ export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('/signup')
   signup(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
-    return this.authService.signup(authCredentialsDto);
+    return this.authService.signUp(authCredentialsDto);
+  }
+
+  @Post('/signin')
+  signIn(@Body() authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    return this.authService.signIn(authCredentialsDto);
   }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,12 +1,28 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { PGError } from '../postgres-errors/error-codes.enum';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const task = this.create({ username, password });
-    await this.save(task);
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const task = this.create({ username, password: hashedPassword });
+    try {
+      await this.save(task);
+    } catch (error) {
+      if (error.code === PGError.UNIQUE_VIOLATION) {
+        throw new ConflictException('Username already exists.');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
