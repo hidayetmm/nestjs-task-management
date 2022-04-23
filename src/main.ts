6d8e5f42306ffed3a1f6bd7c5ad04c3218b9565f
Hidayat <hidayetmmdff@gmail.com>
2022-04-24 00:25:06 +0400

    swagger setup:

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './transform.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   const port = process.env.PORT;
+
+  const config = new DocumentBuilder()
+    .setTitle('Task management')
+    .setDescription('User can sign up/sign in, CRUD tasks')
+    .setVersion('1.0')
+    // .addTag('')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
 }
 bootstrap();
