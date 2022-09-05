ef5641a2f5179d9572369675867564ba09ddc1e2
Hidayat <hidayetmmdff@gmail.com>
2022-09-05 17:54:09 +0400

    cors for localhost

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors({ origin: ['http://localhost:3000'] });
   await app.listen(port);
 }
 bootstrap();
